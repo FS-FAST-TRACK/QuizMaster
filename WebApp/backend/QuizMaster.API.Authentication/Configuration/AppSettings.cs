e8cc5d2e5b6f0ed31c418376be5b6ff8914e5bde
JayMar921 <jayharronabejar@gmail.com>
2023-10-17 21:59:47 +0800

    feat: Implemented a strongly typed settings object

---
+++ WebApp/backend/QuizMaster.API.Authentication/Configuration/AppSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace QuizMaster.API.Authentication.Configuration
+{
+    public class AppSettings
+    {
+        public string JWTSecret { get; set; } = string.Empty;
+        public int IntExpireHour { get; set; } = 1;
+    }
+}
