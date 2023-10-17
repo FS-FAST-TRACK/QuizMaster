ff441e414c61dc689c18142397ca07eb45e7b7b2
JayMar921 <jayharronabejar@gmail.com>
2023-10-17 22:01:26 +0800

    feat: Added Auth Controller's Models

---
+++ WebApp/backend/QuizMaster.API.Authentication/Models/AuthRequest.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace QuizMaster.API.Authentication.Models
+{
+    public class AuthRequest
+    {
+        public string Username { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+
+        [Required]
+        public string Password { get; set; } = string.Empty;
+
+
+    }
+}
---
+++ WebApp/backend/QuizMaster.API.Authentication/Models/AuthResponse.cs
@@ -0,0 +1,7 @@
+﻿namespace QuizMaster.API.Authentication.Models
+{
+    public class AuthResponse
+    {
+        public string? Token { get; set; } = string.Empty;
+    }
+}
