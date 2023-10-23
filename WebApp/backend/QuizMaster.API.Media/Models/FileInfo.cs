211c349f71b42d8ce97a878e368c704a351536bd
JayMar921 <jayharronabejar@gmail.com>
2023-10-23 17:12:46 +0800

    feat: Added FileInfo model, used for controller

---
+++ WebApp/backend/QuizMaster.API.Media/Models/FileInfo.cs
@@ -0,0 +1,11 @@
+﻿namespace QuizMaster.API.Media.Models
+{
+    public class FileInfo
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string FileType { get; set; } = string.Empty;
+        public double FileSize { get; set;} = 0;
+        public string FileName { get; set; } = string.Empty;
+        public DateTime DateCreated { get; set; } = DateTime.Now;
+    }
+}
