1de8af449ec15808118d169e4d64cee2c120ee82
JayMar921 <jayharronabejar@gmail.com>
2023-10-24 18:19:14 +0800

    feat: Implemented Strongly typed settings object for AccountAPI

---
+++ WebApp/backend/QuizMaster.API.Account/Configuration/ApplicationSettings.cs
@@ -0,0 +1,10 @@
+﻿namespace QuizMaster.API.Account.Configuration
+{
+    public class ApplicationSettings
+    {
+        public string RabbitMq_Account_ExchangeName { get; set; }
+        public string RabbitMq_Account_RequestQueueName { get; set; }
+        public string RabbitMq_Account_ResponseQueueName { get; set; }
+        public string RabbitMq_Hostname { get; set; }
+    }
+}
