0e4e923728da5bf36124008e129cb4a634edf84b
JayMar921 <jayharronabejar@gmail.com>
2023-10-24 18:18:19 +0800

    feat: AuthRequest added for RabbitMQ Request

---
+++ WebApp/backend/QuizMaster.Library.Common/Models/Services/AuthRequest.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace QuizMaster.Library.Common.Models.Services
+{
+    public class AuthRequest
+    {
+        public string Username { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+
+        [Required]
+        public string Password { get; set; } = string.Empty;
+    }
+}
