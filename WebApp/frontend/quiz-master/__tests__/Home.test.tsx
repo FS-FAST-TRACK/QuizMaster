9e1957685ce6ad057609c1823bb173c4b0bcdeed
Reynald Sampelo <reynaldsampelo@gmail.com>
2023-10-25 17:39:45 +0800

    fix: added custom render for mantine provider to be in the component tree

--- WebApp/frontend/quiz-master/__tests__/Home.test.tsx
+++ WebApp/frontend/quiz-master/__tests__/Home.test.tsx
@@ -1,10 +1,11 @@
 import Home from "@/app/page";
-import { render, screen } from "@testing-library/react";
-
-it("Should have Docs", () => {
+import { screen } from "@testing-library/react";
+import { MantineProvider } from "@mantine/core";
+import { render } from "@/test-utils/render";
+it("Should have Username", () => {
 	render(<Home />); // arrange
 
-	const elem = screen.getByText("Docs"); // act
+	const elem = screen.getByText("Username"); // act
 
 	expect(elem).toBeInTheDocument(); // assert
 });
---
+++ WebApp/frontend/quiz-master/test-utils/render.tsx
@@ -0,0 +1,11 @@
+// ./test-utils/render.tsx
+import { render as testingLibraryRender } from "@testing-library/react";
+import { MantineProvider } from "@mantine/core";
+
+export function render(ui: React.ReactNode) {
+	return testingLibraryRender(<>{ui}</>, {
+		wrapper: ({ children }: { children: React.ReactNode }) => (
+			<MantineProvider>{children}</MantineProvider>
+		),
+	});
+}
