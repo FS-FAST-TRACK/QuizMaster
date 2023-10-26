8c9e92412e68f2a5d211bd520b1c88db521dc847
Reynald Sampelo <reynaldsampelo@gmail.com>
2023-10-26 21:25:00 +0800

    feat: updated login form

--- WebApp/frontend/quiz-master/app/page.tsx
+++ WebApp/frontend/quiz-master/app/page.tsx
@@ -1,9 +1,23 @@
-import LoginForm from "@/components/login-form";
+import LoginForm from "@/components/LoginForm";
+import LogoutButton from "@/components/logout-button";
+import { Button } from "@mantine/core";
+import { getServerSession } from "next-auth";
+import { signOut } from "next-auth/react";
+import { cookies } from "next/headers";
 
-export default function Home() {
+export default async function Home() {
+	const session = await getServerSession();
+
 	return (
-		<main className="flex min-h-screen flex-col items-center justify-between p-24">
-			<LoginForm />
+		<main className="flex min-h-screen flex-col items-center justify-center h-screen p-24 bg-gradient-to-tr from-30% from-[#17A14B] to-[#1BD260]">
+			{session ? (
+				<div className="text-sm text-stone-500 flex flex-col">
+					logged in as: @{session.user.name}
+					<LogoutButton />
+				</div>
+			) : (
+				<LoginForm />
+			)}
 		</main>
 	);
 }
---
+++ WebApp/frontend/quiz-master/components/LoginForm.tsx
@@ -0,0 +1,113 @@
+"use client";
+
+import {
+	Button,
+	Container,
+	PasswordInput,
+	Text,
+	TextInput,
+} from "@mantine/core";
+import { isNotEmpty, useForm } from "@mantine/form";
+import logo from "/public/quiz-master-logo.png";
+import { signIn } from "next-auth/react";
+import Image from "next/image";
+import "./LoginForm.module.css";
+const LoginForm = () => {
+	const form = useForm({
+		initialValues: {
+			username: "",
+			password: "",
+		},
+		validate: {
+			username: isNotEmpty("Username is required"),
+			password: isNotEmpty("Password is required"),
+		},
+	});
+	const login = async (username, password) => {
+		const response = await fetch(
+			`${process.env.QUIZMASTER_GATEWAY}/auth/login`,
+			{
+				method: "POST",
+				body: JSON.stringify({ username, password }),
+				credentials: "include",
+				headers: {
+					"Content-Type": "application/json",
+				},
+			}
+		);
+		const data = await response.json();
+		console.log(data);
+		await signIn("credentials", {
+			jwt: data.token,
+		});
+	};
+
+	return (
+		<div className="bg-white p-[64px] w-[450px] rounded-[16px] flex flex-col gap-[32px]  items-center shadow-[0px_4px_4px_rgba(0,0,0,.25)]">
+			<Image
+				className=""
+				src={logo}
+				alt="QuizMaster Logo"
+				width={100}
+				height={100}
+			/>
+			<p className="font-semibold text-[24px] ">Login to your account</p>
+			<form
+				className="space-y-5 w-full flex-col flex gap-[40px]"
+				onSubmit={form.onSubmit((e) => {
+					login(e.username, e.password);
+				})}
+			>
+				<div className="flex flex-col gap-[15px]">
+					<Text size="16px" fw="500">
+						{" "}
+						Username
+					</Text>
+
+					<TextInput
+						radius="6px"
+						placeholder="Username"
+						name="username"
+						id="username"
+						{...form.getInputProps("username")}
+					/>
+
+					<Text size="16px" fw="500">
+						{" "}
+						Password
+					</Text>
+					<PasswordInput
+						radius="6px"
+						placeholder="Password"
+						name="password"
+						id="password"
+						{...form.getInputProps("password")}
+					/>
+					<p className="text-sm self-end w-full flex justify-end ">
+						Forgot password?
+					</p>
+				</div>
+				<div className=" flex flex-col justify-center items-center gap-[8px]">
+					<Button
+						size="18px"
+						h="52px"
+						fw={700}
+						color="#FF6633"
+						type="submit"
+						fullWidth
+					>
+						Login
+					</Button>
+					<p className="text-sm">
+						Don't have an account yet?{" "}
+						<span className="font-medium hover:underline cursor-pointer">
+							Sign up
+						</span>
+					</p>
+				</div>
+			</form>
+		</div>
+	);
+};
+
+export default LoginForm;
