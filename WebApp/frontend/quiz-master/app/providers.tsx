48440fee978e43bf7da2d8b706be68bbf8783275
Reynald Sampelo <reynaldsampelo@gmail.com>
2023-10-26 22:10:43 +0800

    feat: integrated nextauth to the project and wrapped nodes with the AuthProvider component

---
+++ WebApp/frontend/quiz-master/app/providers.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import { SessionProvider } from "next-auth/react";
+import { PropsWithChildren } from "react";
+
+import { Session } from "next-auth";
+
+type AuthProviderProps = {
+	children: React.ReactNode;
+	session?: Session | null;
+};
+
+export const AuthProvider = ({
+	children,
+	session,
+}: PropsWithChildren<AuthProviderProps>) => {
+	return <SessionProvider session={session}>{children}</SessionProvider>;
+};
