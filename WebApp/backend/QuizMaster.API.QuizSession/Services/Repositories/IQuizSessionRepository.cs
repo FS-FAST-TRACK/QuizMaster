08dd8315ebf21785396179ab3c3513ad4df769a2
Jhon Mark Acopiado <jacopiado@fullscale.io>
2023-11-14 20:46:03 +0800

    feat: Added QuizSession interface.

---
+++ WebApp/backend/QuizMaster.API.QuizSession/Services/Repositories/IQuizSessionRepository.cs
@@ -0,0 +1,25 @@
+﻿using QuizMaster.Library.Common.Entities.Questionnaire;
+using QuizMaster.Library.Common.Helpers.Quiz;
+
+namespace QuizMaster.API.QuizSession.Services.Repositories
+{
+	public interface IQuizSessionRepository
+	{
+		Task<IEnumerable<Question>> GetAllQuestionsAsync();
+		Task<Question?> GetQuestionAsync(int id);
+
+		Task<IEnumerable<QuestionCategory>> GetAllCategoriesAsync();
+		Task<QuestionCategory?> GetCategoryAsync(int id);
+
+
+		Task<IEnumerable<QuestionDifficulty>> GetAllDifficultiesAsync();
+		Task<QuestionDifficulty?> GetDifficultyAsync(int id);
+
+
+		Task<IEnumerable<QuestionType>> GetAllTypesAsync();
+		Task<QuestionType?> GetTypeAsync(int id);
+
+		Task<IEnumerable<QuestionDetail>> GetQuestionDetailsAsync(int qId);
+		Task<IEnumerable<QuestionDetail>> GetQuestionDetailByDetailTypeAsync(int qId, int detailTypeId);
+	}
+}
