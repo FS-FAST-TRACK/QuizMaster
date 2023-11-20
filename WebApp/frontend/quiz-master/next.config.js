998e7c15a5298598df23fb89547395f311fd1281
Jhon Mark Acopiado <jacopiado@fullscale.io>
2023-11-20 23:07:45 +0800

    feat: Add prettier.

--- WebApp/frontend/quiz-master/next.config.js
+++ WebApp/frontend/quiz-master/next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  env: {
+    QUIZMASTER_GATEWAY: process.env.QUIZMASTER_GATEWAY,
+    QUIZMASTER_QUIZ: process.env.QUIZMASTER_QUIZ,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
