ec30ec18d1f3b2407eedc717caaaf39e44ee3af0
Jhon Mark Acopiado <jacopiado@fullscale.io>
2023-11-21 16:35:30 +0800

    fix: Fix dashboard layout.

--- WebApp/frontend/quiz-master/app/dashboard/layout.tsx
+++ WebApp/frontend/quiz-master/app/dashboard/layout.tsx
@@ -1,8 +1,6 @@
 import SideNav from "@/components/Commons/navbars/sidenav";
-import { useState } from "react";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
-    const [isSideBarOpen, setIsSideBarOpen] = useState(true);
     return (
         <div className="flex h-screen flex-row md:overflow-hidden">
             <div className="flex-none transition w-64 ">
