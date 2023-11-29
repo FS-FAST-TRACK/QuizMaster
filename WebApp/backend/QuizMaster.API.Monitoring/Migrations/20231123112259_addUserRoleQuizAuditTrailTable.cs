d70f2d7b8cbda6f3ba89b8b4a60a849cdd60c3b4
Nealford Sotes <nsotes@fullscale.io>
2023-11-29 17:49:18 +0800

    Feat: Add quiz category audit trail

---
+++ WebApp/backend/QuizMaster.API.Monitoring/Migrations/20231123112259_addUserRoleQuizAuditTrailTable.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace QuizMaster.API.Monitoring.Migrations
+{
+    /// <inheritdoc />
+    public partial class addUserRoleQuizAuditTrailTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "UserRole",
+                table: "QuizAuditTrails",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UserRole",
+                table: "QuizAuditTrails");
+        }
+    }
+}
---
+++ WebApp/backend/QuizMaster.API.Monitoring/Migrations/20231128084632_addUserNameQuizAuditTrailTable.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace QuizMaster.API.Monitoring.Migrations
+{
+    /// <inheritdoc />
+    public partial class addUserNameQuizAuditTrailTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "UserName",
+                table: "QuizAuditTrails",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UserName",
+                table: "QuizAuditTrails");
+        }
+    }
+}
