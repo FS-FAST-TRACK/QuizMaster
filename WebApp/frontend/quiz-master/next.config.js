083b8c64491d68ccacecd30489f70f464fa587bc
Jhon Mark Acopiado <jacopiado@fullscale.io>
2023-12-04 12:15:52 +0800

    ref: Applied prettier.

--- WebApp/frontend/quiz-master/next.config.js
+++ WebApp/frontend/quiz-master/next.config.js
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  env: {
-    QUIZMASTER_GATEWAY: process.env.QUIZMASTER_GATEWAY,
-    QUIZMASTER_QUIZ: process.env.QUIZMASTER_QUIZ,
-    QUIZMASTER_MEDIA: process.env.QUIZMASTER_MEDIA,
-  },
+    env: {
+        QUIZMASTER_GATEWAY: process.env.QUIZMASTER_GATEWAY,
+        QUIZMASTER_QUIZ: process.env.QUIZMASTER_QUIZ,
+        QUIZMASTER_MEDIA: process.env.QUIZMASTER_MEDIA,
+    },
 };
 
 module.exports = nextConfig;
