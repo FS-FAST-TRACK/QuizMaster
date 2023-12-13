0b295ac637e4c0848ec8fca0817599fb0a968d22
Harold Cuico <htarimanc@gmail.com>
2023-12-13 23:17:34 +0800

    Created Initial UI for the Quiz session

---
+++ WebApp/frontend/quiz_session/tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        wall: "#18A44C",
+        secondary_text: "#8D8D8D",
+        button: "#17A14B",
+      },
+    },
+  },
+  plugins: [],
+};
