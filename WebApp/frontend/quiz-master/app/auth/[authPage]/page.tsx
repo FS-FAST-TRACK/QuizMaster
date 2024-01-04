49ec798dfb10d1d6f3026f1e66a54c07512e3cf2
Jhon Mark L. Acopiado <jacopiado@fullscale.io>
2024-01-04 17:58:46 +0800

    feat: Added error boundary for unsuccesful comunication with the api server.'

--- WebApp/frontend/quiz-master/app/auth/[authPage]/page.tsx
+++ WebApp/frontend/quiz-master/app/auth/[authPage]/page.tsx
@@ -1,17 +1,22 @@
 "use client";
 
 import LoginForm from "@/components/login-form";
+import ErrorContainer from "@/components/pages/ErrorContainer";
+import ErrorBoundary from "@/components/pages/ErrorContainer";
 import RegisterForm from "@/components/register-form";
 import { useSession } from "next-auth/react";
 import { redirect } from "next/navigation";
+import { useSearchParams } from "next/navigation";
 
 export default function Page({
     params,
 }: {
-    params: { authPage: "login" | "signup" | undefined; callbackUrl: string };
+    params: { authPage: "login" | "signup" | undefined };
 }) {
+    const searchParams = useSearchParams();
+
     const authPage = params.authPage || "login";
-    const callbackUrl = params.callbackUrl || "/dashboard";
+    const callbackUrl = searchParams.get("callbackUrl") || "/dashboard";
     const { status } = useSession();
 
     if (status === "loading") {
@@ -22,17 +27,19 @@ export default function Page({
     }
 
     return (
-        <div className="flex min-h-screen flex-col items-center justify-center h-screen p-10 md:p-24 bg-gradient-to-tr from-30% from-[#17A14B] to-[#1BD260]">
-            <div className={`${authPage === "login" ? "" : "hidden"}`}>
-                <LoginForm callbackUrl={callbackUrl} />
+        <ErrorContainer>
+            <div className="flex min-h-screen flex-col items-center justify-center h-screen p-10 md:p-24 bg-gradient-to-tr from-30% from-[#17A14B] to-[#1BD260]">
+                <div className={`${authPage === "login" ? "" : "hidden"}`}>
+                    <LoginForm callbackUrl={callbackUrl} />
+                </div>
+                <div
+                    className={`${
+                        authPage === "signup" ? "" : "hidden"
+                    } w-full flex justify-center`}
+                >
+                    <RegisterForm />
+                </div>
             </div>
-            <div
-                className={`${
-                    authPage === "signup" ? "" : "hidden"
-                } w-full flex justify-center`}
-            >
-                <RegisterForm />
-            </div>
-        </div>
+        </ErrorContainer>
     );
 }
