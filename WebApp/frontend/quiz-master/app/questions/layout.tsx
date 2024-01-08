e1c9a8aacefd0ca0619633987792c6143395ccfd
Jhon Mark L. Acopiado <jacopiado@fullscale.io>
2024-01-08 14:37:37 +0800

    ref: Refactor questions page to use the custom response.

--- WebApp/frontend/quiz-master/app/questions/layout.tsx
+++ WebApp/frontend/quiz-master/app/questions/layout.tsx
@@ -5,27 +5,37 @@ import SideNav from "@/components/Commons/navbars/sidenav";
 import { useQuestionCategoriesStore } from "@/store/CategoryStore";
 import { useQuestionDifficultiesStore } from "@/store/DifficultyStore";
 import { useQuestionTypesStore } from "@/store/TypeStore";
-import { Suspense, useEffect } from "react";
+import { Suspense, useCallback, useEffect } from "react";
 import Loading from "./loading";
 import { getAllCategories } from "@/lib/hooks/category";
 import { getAllDifficulties } from "@/lib/hooks/difficulty";
 import { getAllTypes } from "@/lib/hooks/type";
+import ErrorContainer from "@/components/pages/ErrorContainer";
+import { useErrorRedirection } from "@/utils/errorRedirection";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
     const { setQuestionCategories } = useQuestionCategoriesStore();
     const { setQuestionDifficulties } = useQuestionDifficultiesStore();
     const { setQuestionTypes } = useQuestionTypesStore();
+    const { redirectToError } = useErrorRedirection();
+
+    const populateData = useCallback(async () => {
+        try {
+            const categoriesRes = await getAllCategories();
+            setQuestionCategories(categoriesRes.data);
+
+            const difficultiesRes = await getAllDifficulties();
+            setQuestionDifficulties(difficultiesRes.data);
+
+            const typesRes = await getAllTypes();
+            setQuestionTypes(typesRes.data);
+        } catch (error) {
+            redirectToError();
+        }
+    }, []);
 
     useEffect(() => {
-        getAllCategories().then((res) => {
-            setQuestionCategories(res.data);
-        });
-        getAllDifficulties().then((res) => {
-            setQuestionDifficulties(res.data);
-        });
-        getAllTypes().then((res) => {
-            setQuestionTypes(res.data);
-        });
+        populateData();
     }, [setQuestionCategories, setQuestionDifficulties, setQuestionTypes]);
 
     return (
@@ -33,7 +43,9 @@ export default function Layout({ children }: { children: React.ReactNode }) {
             <SideNav />
             <div className="grow overflow-y-auto flex flex-col bg-[#F8F9FA]">
                 <PageHeader>Questions</PageHeader>
-                <Suspense fallback={<Loading />}>{children}</Suspense>
+                <ErrorContainer>
+                    <Suspense fallback={<Loading />}>{children}</Suspense>
+                </ErrorContainer>
             </div>
         </div>
     );
