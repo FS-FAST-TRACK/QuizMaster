c6d9bb2fbf0b9337d21bc25434fbd27dae238052
Alyssa <ajumapao@fullscale.io>
2024-01-11 21:04:58 +0800

    Feat: Added Contact Us Page for admin and user side (Template only)

---
+++ WebApp/frontend/quiz-master/app/contact-us/layout.tsx
@@ -0,0 +1,15 @@
+import HeadNav from "@/components/Commons/navbars/head-nav";
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+    return (
+        <div className="flex flex-col w-full h-full py-8 px-24 text-[#3C3C3C] bg-gradient-to-r from-[#17A14B] to-[#1AC159] gap-20 overflow-auto">
+            <HeadNav />
+            {children}
+            <div>
+                <p className="text-sm text-white">
+                    Copyright 2023 Ⓒ QuizMaster
+                </p>
+            </div>
+        </div>
+    );
+}
