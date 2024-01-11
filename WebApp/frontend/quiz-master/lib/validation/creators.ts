167e5c7d367d25cbf42f3a17241f6b73c0d20bfa
Jhon Mark L. Acopiado <jacopiado@fullscale.io>
2024-01-11 20:02:21 +0800

    ref: Run prettier.

--- WebApp/frontend/quiz-master/lib/validation/creators.ts
+++ WebApp/frontend/quiz-master/lib/validation/creators.ts
@@ -1,33 +1,41 @@
 import { InputValidator } from "./regex";
 
-const minimumCharacter = (minLen: number, error?: string) : InputValidator => {
+const minimumCharacter = (minLen: number, error?: string): InputValidator => {
     return {
-        ErrorMessage: error? error:  `Must contain at least ${minLen} characters.`,
-        Regex: new RegExp(`.{${minLen},}`)
-    }
-}
+        ErrorMessage: error
+            ? error
+            : `Must contain at least ${minLen} characters.`,
+        Regex: new RegExp(`.{${minLen},}`),
+    };
+};
 
-const maximumCharater = (maxLen: number, error?: string) : InputValidator => {
+const maximumCharater = (maxLen: number, error?: string): InputValidator => {
     return {
-        ErrorMessage: error? error:  `Must contain at least ${maxLen} characters.`,
-        Regex: new RegExp(`.{0,${maxLen}}`)
-    }
-}
+        ErrorMessage: error
+            ? error
+            : `Must contain at least ${maxLen} characters.`,
+        Regex: new RegExp(`.{0,${maxLen}}`),
+    };
+};
 
-const emptyValidator = () : InputValidator=> {
+const emptyValidator = (): InputValidator => {
     return {
         ErrorMessage: "",
-        Regex: new RegExp(".*")
-    }
-}
+        Regex: new RegExp(".*"),
+    };
+};
 
-export const validatorFactory = (len: number, type: "min" | "max", error?: string) => {
+export const validatorFactory = (
+    len: number,
+    type: "min" | "max",
+    error?: string
+) => {
     switch (type) {
         case "max":
-            return maximumCharater(len, error)
+            return maximumCharater(len, error);
         case "min":
-            return minimumCharacter(len, error)
-            default: 
-            return emptyValidator()
+            return minimumCharacter(len, error);
+        default:
+            return emptyValidator();
     }
-}
+};
