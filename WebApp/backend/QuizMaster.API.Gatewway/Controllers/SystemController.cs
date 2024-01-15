d20b401e8e5051513ed0c89219f574694370df42
JayMar921 <jayharronabejar@gmail.com>
2024-01-15 16:32:36 +0800

    Feature: Added Class Models for System CRUD modules

---
+++ WebApp/backend/QuizMaster.API.Gatewway/Controllers/SystemController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace QuizMaster.API.Gateway.Controllers
+{
+    public class SystemController : Controller
+    {
+        
+    }
+}
---
+++ WebApp/backend/QuizMaster.API.Gatewway/Models/System/ReviewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace QuizMaster.API.Gateway.Models.System
+{
+    public class ReviewModel
+    {
+        public int? UserId { get; set; } = null;
+        public int StarRating { get; set; } = 0;
+        public string Comment { get; set; } = string.Empty;
+    }
+
+    public class Reviews : ReviewModel
+    {
+        [Key]
+        public int Id { get; set; } = 0;
+    }
+}
