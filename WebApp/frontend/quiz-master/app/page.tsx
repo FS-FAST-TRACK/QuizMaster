e23b5d8479320316e0b58ca0de9296e2c1c0701e
kinloveko <delacruzk4817@gmail.com>
2024-02-23 17:03:29 +0800

    added the nav bar in the home page

--- WebApp/frontend/quiz-master/app/page.tsx
+++ WebApp/frontend/quiz-master/app/page.tsx
@@ -1,14 +1,13 @@
 import LoginForm from "@/components/login-form";
 import LogoutButton from "@/components/logout-button";
-import { Button } from "@mantine/core";
 import { getServerSession } from "next-auth";
-import { signOut } from "next-auth/react";
-import { cookies } from "next/headers";
+import HeadNav from "@/components/Commons/navbars/head-nav";
 
 export default async function Home() {
     const session = await getServerSession();
     return (
-        <main className="flex min-h-screen flex-col items-center justify-center h-screen p-24 bg-gradient-to-tr from-30% from-[#17A14B] to-[#1BD260]">
+        <main className="flex flex-col w-full h-full py-8 px-24 text-[#3C3C3C] bg-gradient-to-r from-[#17A14B] to-[#1AC159] gap-20 overflow-auto">
+        <HeadNav />
             {session ? (
                 <div className="text-sm text-stone-500 flex flex-col">
                     logged in as: @{session.user.name}
@@ -17,6 +16,8 @@ export default async function Home() {
             ) : (
                 <LoginForm callbackUrl="/dashboard" />
             )}
+            
+        
         </main>
     );
 }
