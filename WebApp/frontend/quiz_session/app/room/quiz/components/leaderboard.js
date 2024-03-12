47aaebc3563c7d74ce0e196a171be827cae50fa2
Raphael Jess Bacordio <91072520+raisondetre1207@users.noreply.github.com>
2024-03-12 12:46:19 +0800

    Feat: Add animation to leaderboard

--- WebApp/frontend/quiz_session/app/room/quiz/components/leaderboard.js
+++ WebApp/frontend/quiz_session/app/room/quiz/components/leaderboard.js
@@ -1,6 +1,7 @@
 import React from "react";
 import user from "@/public/icons/user.png";
 import Image from "next/image";
+import { AnimateSlideInFromBottom } from "./AnimateSlideInFromBottom";
 
 export default function Leaderboard({ leaderBoard }) {
   const getRankColorClass = (index) => {
@@ -17,36 +18,38 @@ export default function Leaderboard({ leaderBoard }) {
   };
   return (
     <div className="h-full w-full flex justify-center items-center ">
-      <div className="w-[90%] sm:w-[75%] md:w-[45%] lg:w-[50%] xl:w-1/3 h-3/4 max-w-[460px] bg-white p-5 overflow-auto space-y-5 rounded-lg shadow-2xl shadow-green-600">
-        <div className="flex flex-row justify-between">
-          <div className="font-bold">Leaderboard</div>
-          <div className="flex flex-row space-x-1">
-            <Image src={user} width={23} height={20} />
-            <div>{leaderBoard.length}</div>
+      <AnimateSlideInFromBottom className="w-[90%] sm:w-[75%] md:w-[45%] lg:w-[50%] xl:w-1/3 h-3/4 max-w-[460px] bg-white  rounded-lg shadow-2xl shadow-green-600">
+        <div className="p-5 overflow-auto space-y-5">
+          <div className="flex flex-row justify-between">
+            <div className="font-bold">Leaderboard</div>
+            <div className="flex flex-row space-x-1">
+              <Image src={user} width={23} height={20} />
+              <div>{leaderBoard.length}</div>
+            </div>
+          </div>
+          <div className="space-y-3">
+            <div className="flex flex-row p-2 rounded-lg">
+              <div className=" flex-grow font-bold text-slate-500">
+                PARTICIPANTS
+              </div>
+              <div className="text-score_result">SCORE</div>
+            </div>
+          </div>
+          <div className="space-y-3">
+            {leaderBoard.map((participant, index) => (
+              <div
+                className={` ${getRankColorClass(
+                  index
+                )} flex flex-row p-2 rounded-lg`}
+                key={index}
+              >
+                <div className=" flex-grow font-bold">{participant.name}</div>
+                <div className="text-score_result">{participant.score}</div>
+              </div>
+            ))}
           </div>
         </div>
-        <div className="space-y-3">
-          <div className="flex flex-row p-2 rounded-lg">
-            <div className=" flex-grow font-bold text-slate-500">
-              PARTICIPANTS
-            </div>
-            <div className="text-score_result">SCORE</div>
-          </div>
-        </div>
-        <div className="space-y-3">
-          {leaderBoard.map((participant, index) => (
-            <div
-              className={` ${getRankColorClass(
-                index
-              )} flex flex-row p-2 rounded-lg`}
-              key={index}
-            >
-              <div className=" flex-grow font-bold">{participant.name}</div>
-              <div className="text-score_result">{participant.score}</div>
-            </div>
-          ))}
-        </div>
-      </div>
+      </AnimateSlideInFromBottom>
     </div>
   );
 }
